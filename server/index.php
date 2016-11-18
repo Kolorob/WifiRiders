cfde163689377c1e55e069fe85f4188c19361c35
Tawseef Khan <taw_khan@yahoo.com>
2016-11-18 23:10:29 +0600

    init

---
+++ server/index.php
@@ -0,0 +1,18 @@
+<?php
+    require_once("config.php");
+    require_once("lib/database.php");
+?>
+try{
+	$db = Database::get();
+	$sucess = $db->insert("customer", array("name"=>$_GET["name"], "mac"=>$_GET["mac"], "phone"=>$_GET["phone"]));
+	if($sucess) echo "sucess";
+	else echo "failed";
+}
+catch($e){
+	echo "failed";
+}
+
+
+
+
+
