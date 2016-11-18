5aa61ed657aa5d040dcdc5e220ca0c11a111c8ba
Tawseef Khan <taw_khan@yahoo.com>
2016-11-18 23:30:59 +0600

    al

--- server/register.php
+++ server/register.php
@@ -1,7 +1,7 @@
 <?php
     require_once("config.php");
     require_once("lib/database.php");
-?>
+
 try{
 	$db = Database::get();
 	$sucess = $db->insert("customer", array("name"=>$_GET["name"], "mac"=>$_GET["mac"], "phone"=>$_GET["phone"]));
@@ -11,6 +11,8 @@ try{
 catch($e){
 	echo "failed";
 }
+
+?>
 
 
 
